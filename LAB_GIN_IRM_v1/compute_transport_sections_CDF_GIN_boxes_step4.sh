876cc4cb2f7303d09daf8a608746bd22e00ff473
enerle <97646102+enerle@users.noreply.github.com>
2025-05-11 12:55:11 +0200

    Add files via upload

---
+++ LAB_GIN_IRM_v1/compute_transport_sections_CDF_GIN_boxes_step4.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+#
+module load NCO/5.1.3-hpc1-gcc-2022a-eb
+module load CDO/2.3.0-eccodes-aec-cmor-hpc1-intel-2023a-eb
+module load netCDF-HDF5-utils/4.9.2-1.12.2-hpc1-intel-2023a-eb
+
+datadir="/nobackup/rossby26/proj/rossby/joint_exp/ligarctic/cdftools/CDFTOOLS/analysis"
+cdfdir="/nobackup/rossby26/proj/rossby/joint_exp/ligarctic/cdftools/CDFTOOLS/bin"
+
+for run in 'Cdio' #'Hist' 'Hist_SSP245' 'Hspu' 'Cfix' 'Cfx2'
+do
+if [ ${run} = 'Hspu' ]
+then
+  years='2241_2379'
+  yr1='2241'
+  yr2='2379'
+elif [ ${run} = 'Cfix' ]
+then
+  years='1917_2057'
+  yr1='1917'
+  yr2='2057'
+elif [ ${run} = 'Cfx2' ]
+then
+  years='1885_2018'
+  yr1='1885'
+  yr2='2018'
+elif [ ${run} = 'Hist' ]
+then
+  years='1851_2014'
+  yr1='1851'
+  yr2='2014'
+elif [ ${run} = 'Hist_SSP245' ]
+then
+  years='2015_2100'
+  yr1='2015'
+  yr2='2100'
+elif [ ${run} = 'Cdio' ]
+then
+  years='1851_2042'
+  yr1='1851'
+  yr2='2042'
+fi
+
+cd ${datadir}/Data/data_700_1000/data_${run}/VTRP_DEP/
+
+echo ------------------------------------------------------------
+echo ${datadir}/Data/data_700_1000/data_${run}/VTRP_DEP/
+echo ------------------------------------------------------------
+
+for zz in `seq 0 1`
+do
+
+  mylist_east_mtrp=()
+  mylist_west_mtrp=()
+  mylist_north_mtrp=()
+  mylist_south_mtrp=()
+  
+  mylist_east_ptrp=()
+  mylist_west_ptrp=()
+  mylist_north_ptrp=()
+  mylist_south_ptrp=()
+
+  for year in `seq $yr1 $yr2`
+  do
+    mylist_east_mtrp+=(${run}_${year}_GIN_East_mtrp_dep_${zz}_ym.nc)
+    mylist_west_mtrp+=(${run}_${year}_GIN_West_mtrp_dep_${zz}_ym.nc)
+    mylist_north_mtrp+=(${run}_${year}_GIN_North_mtrp_dep_${zz}_ym.nc)
+    mylist_south_mtrp+=(${run}_${year}_GIN_South_mtrp_dep_${zz}_ym.nc)
+
+    mylist_east_ptrp+=(${run}_${year}_GIN_East_ptrp_dep_${zz}_ym.nc)
+    mylist_west_ptrp+=(${run}_${year}_GIN_West_ptrp_dep_${zz}_ym.nc)
+    mylist_north_ptrp+=(${run}_${year}_GIN_North_ptrp_dep_${zz}_ym.nc)
+    mylist_south_ptrp+=(${run}_${year}_GIN_South_ptrp_dep_${zz}_ym.nc)
+  done
+
+  ncecat ${mylist_east_mtrp[*]} ${run}_${years}_GIN_East_mtrp_dep_${zz}_ym.nc
+  ncecat ${mylist_west_mtrp[*]} ${run}_${years}_GIN_West_mtrp_dep_${zz}_ym.nc
+  ncecat ${mylist_north_mtrp[*]} ${run}_${years}_GIN_North_mtrp_dep_${zz}_ym.nc
+  ncecat ${mylist_south_mtrp[*]} ${run}_${years}_GIN_South_mtrp_dep_${zz}_ym.nc
+
+  ncecat ${mylist_east_ptrp[*]} ${run}_${years}_GIN_East_ptrp_dep_${zz}_ym.nc
+  ncecat ${mylist_west_ptrp[*]} ${run}_${years}_GIN_West_ptrp_dep_${zz}_ym.nc
+  ncecat ${mylist_north_ptrp[*]} ${run}_${years}_GIN_North_ptrp_dep_${zz}_ym.nc
+  ncecat ${mylist_south_ptrp[*]} ${run}_${years}_GIN_South_ptrp_dep_${zz}_ym.nc
+
+mylist_east_mtrp=()
+mylist_west_mtrp=()
+mylist_north_mtrp=()
+mylist_south_mtrp=()
+  
+mylist_east_ptrp=()
+mylist_west_ptrp=()
+mylist_north_ptrp=()
+mylist_south_ptrp=()
+
+ncrename -h -O -d record,time ${run}_${years}_GIN_East_mtrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_West_mtrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_North_mtrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_South_mtrp_dep_${zz}_ym.nc
+
+ncrename -h -O -d record,time ${run}_${years}_GIN_East_ptrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_West_ptrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_North_ptrp_dep_${zz}_ym.nc
+ncrename -h -O -d record,time ${run}_${years}_GIN_South_ptrp_dep_${zz}_ym.nc
+
+done  
+done
+
+exit
---
+++ LAB_GIN_IRM_v1/compute_transport_sections_CDF_IRM_boxes_step0.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+module load CDO/2.3.0-eccodes-aec-cmor-hpc1-intel-2023a-eb
+module load netCDF-HDF5-utils/4.9.2-1.12.2-hpc1-intel-2023a-eb
+
+datadir="/nobackup/rossby26/proj/rossby/joint_exp/ligarctic/post-processed_4"
+cdfdir="/nobackup/rossby26/proj/rossby/joint_exp/ligarctic/cdftools/CDFTOOLS/bin"
+
+#for run in 'Hist' 'Hist_SSP245' 'Hspu' 'Cfix' 'Cfx2'
+for run in 'Cdio'
+do
+if [ ${run} = 'Hspu' ]
+then
+  years='2241_2379'
+  yr1='2241'
+  yr2='2379' 
+elif [ ${run} = 'Cfix' ]
+then
+  years='1917_2057'
+  yr1='1917'
+  yr2='2057'
+elif [ ${run} = 'Cfx2' ]
+then
+  years='1885_2018'
+  yr1='1885'
+  yr2='2018'
+elif [ ${run} = 'Hist' ]
+then
+  years='1851_2014'
+  yr1='1851'
+  yr2='2014'
+elif [ ${run} = 'Hist_SSP245' ]
+then
+  years='2015_2100'
+  yr1='2015'
+  yr2='2100'
+elif [ ${run} = 'Cdio' ]
+then
+  years='1851_2042'
+  yr1='1969' #'1851'
+  yr2='2042' #'1968'
+fi
+
+for yy in `seq $yr1 $yr2`
+do
+  for kk in `seq 1 12`
+  do 
+  echo ${run} ${yy} ${kk}
+  ../bin/cdftransport -pm -u ${datadir}/${run}_${yy}0101_${yy}1231_uo_v0.nc -v ${datadir}/${run}_${yy}0101_${yy}1231_vo_v0.nc -noheat -time ${kk} -zlimit 1000 <section_irm.dat
+  rename -v ATL_ ATL_${yy}_month_${kk}_ ATL_*.nc
+  mv ATL_*.nc Data/data_700_1000/data_${run} 
+  done
+done
+
+done
+exit

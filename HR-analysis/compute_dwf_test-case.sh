0a1e548fa3af366b82b1e264e39492de02aa51b4
enerle <97646102+enerle@users.noreply.github.com>
2025-12-12 12:25:29 +0100

    Update compute_dwf_test-case.sh

--- HR-analysis/compute_dwf_test-case.sh
+++ HR-analysis/compute_dwf_test-case.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# test-case
+# test-case using a yearmean file (only one timestep)
 
 module load CDO/2.3.0-eccodes-aec-cmor-hpc1-intel-2023a-eb
 module load netCDF-HDF5-utils/4.9.2-1.12.2-hpc1-intel-2023a-eb
@@ -7,7 +7,7 @@ module load netCDF-HDF5-utils/4.9.2-1.12.2-hpc1-intel-2023a-eb
 run="Hist"
 yr1="1851"
 zz="1000"
-
+kk="1"
 ../bin/cdftransport -pm -u ${run}_${yy}0101_${yy}1231_uo_yearmean_v0.nc -v ${run}_${yy}0101_${yy}1231_vo_yearmean_v0.nc -noheat -time 1 -zlimit ${kk} <sections.dat
 
 exit
